7cf1d5c6953283069025aa52bbfa41ba9ab15c35
Mohamed Kholy <mohamed@Mohameds-iMac.local>
2026-07-07 11:19:22 +0300

    Create SQL DDL for UserPagePermissions table

---
+++ SQLScript/UserPagePermissions.sql
@@ -0,0 +1,28 @@
+USE ERPMega
+GO
+
+-- Create PLS schema if it doesn't exist
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'PLS')
+BEGIN
+    EXEC('CREATE SCHEMA PLS')
+END
+GO
+
+-- Create [PLS].[UserPagePermissions] Table
+IF OBJECT_ID('PLS.UserPagePermissions', 'U') IS NULL
+BEGIN
+    CREATE TABLE [PLS].[UserPagePermissions] (
+        [PermissionID] INT IDENTITY(1,1) PRIMARY KEY,
+        [UserCode] VARCHAR(100) NOT NULL,
+        [PageGroupID] VARCHAR(50) NOT NULL,
+        [CanView] BIT NOT NULL DEFAULT 1,
+        [CanEdit] BIT NOT NULL DEFAULT 0,
+        [CanDelete] BIT NOT NULL DEFAULT 0,
+        [GrantedBy] NVARCHAR(100) NULL,
+        [GrantedDate] DATETIME DEFAULT GETDATE(),
+        CONSTRAINT FK_UserPagePermissions_PagesAndGroups FOREIGN KEY ([PageGroupID]) REFERENCES [PLS].[PagesAndGroups]([PageGroupID])
+    );
+
+    CREATE NONCLUSTERED INDEX IX_UserPagePermissions_User ON [PLS].[UserPagePermissions] (UserCode);
+END
+GO
