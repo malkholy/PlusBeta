519e56959eac15af3afb22c714ad6e9578fe9b66
Mohamed Kholy <mohamed@Mohameds-iMac.local>
2026-07-21 10:34:15 +0300

    fix: correct column names and schema for RegisterRecruitmentTestsPage.sql

--- SQLScript/RegisterRecruitmentTestsPage.sql
+++ SQLScript/RegisterRecruitmentTestsPage.sql
@@ -3,28 +3,39 @@
 -- ==============================================================================
 
 -- 1. Insert into PagesAndGroups if it does not exist
-IF NOT EXISTS (SELECT * FROM [PLS].[PagesAndGroups] WHERE PageGroupID = 'recruitment_tests')
+IF NOT EXISTS (SELECT * FROM [PLS].[PagesAndGroups] WHERE [PageGroupID] = 'recruitment_tests')
 BEGIN
-    INSERT INTO [PLS].[PagesAndGroups] (PageGroupID, ParentGroupID, SystemID, NodeTitle, NodeDescription)
+    INSERT INTO [PLS].[PagesAndGroups] ([PageGroupID], [Label], [Icon], [Description], [IsGroup], [ParentID], [SortOrder])
     VALUES (
-        'recruitment_tests',
-        'recruitment_group',
-        'plus',
-        'Candidate Assessments & Tests',
-        'Manage and generate IQ, English, and other candidate tests'
+        'recruitment_tests', 
+        N'Candidate Tests', 
+        N'📝', 
+        N'Manage and generate candidate assessment tests', 
+        0, 
+        'recruitment_group', 
+        560
     );
 END
 GO
 
 -- 2. Register the Query in QueryMaster to grant access to the GetRecruitmentTests operation
-IF NOT EXISTS (SELECT * FROM [PLS].[QueryMaster] WHERE QueryID = 'Q_RecruitmentTestsGrid')
+DECLARE @QID INT;
+IF NOT EXISTS (SELECT 1 FROM [PLS].[QueryMaster] WHERE [QueryName] = N'Get Recruitment Tests')
 BEGIN
-    INSERT INTO [PLS].[QueryMaster] (QueryID, OperationName, Description, SystemID)
+    INSERT INTO [PLS].[QueryMaster] ([QueryName], [SPName], [Operation], [Description], [QuerySQL], [DatabaseName], [SchemaName], [TableOrViewName], [QueryType], [CreatedBy], [ApiUrl])
     VALUES (
-        'Q_RecruitmentTestsGrid',
-        'GetRecruitmentTests',
-        'Grid data for recruitment tests',
-        'plus'
+        N'Get Recruitment Tests', 
+        N'[PLS].[APIPlusRecruitmentOperation]', 
+        'GetRecruitmentTests', 
+        N'Retrieve list of recruitment tests and their question count', 
+        N'SELECT t.TestID, t.TestTitle, t.TestType, t.CreatedBy, t.CreatedDate, (SELECT COUNT(*) FROM [PLS].[RecruitmentTestQuestions] q WHERE q.TestID = t.TestID) AS QuestionCount FROM [PLS].[RecruitmentTests] t ORDER BY t.TestID DESC;', 
+        'HR', 
+        'PLS', 
+        'RecruitmentTests', 
+        'Grid', 
+        'System', 
+        'https://quick.glcpaints.com:7001/General/GeneralAPI/'
     );
+    SET @QID = SCOPE_IDENTITY();
 END
 GO
